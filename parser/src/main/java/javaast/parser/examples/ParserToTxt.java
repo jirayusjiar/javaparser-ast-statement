619a96db1492737dbd25478e1dca6c848809d23e
PoomPoowit <poompoowit@hotmail.com>
2016-07-08 15:26:47 +0900

    Parser To Txt

--- parser/src/main/java/javaast/parser/examples/ParserToTxt.java
+++ parser/src/main/java/javaast/parser/examples/ParserToTxt.java
@@ -2,6 +2,7 @@ package javaast.parser.examples;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javaast.parser.support.DirExplorer;
 import javaast.parser.support.NodeIterator;
@@ -47,201 +48,203 @@ import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.google.common.base.Strings;
 
-public class Full_Parser {
+public class ParserToTxt {
 
    public static void statementsByLine(File projectDir) {
 	  new DirExplorer((level, path, file) -> path.endsWith(".java"), (level,
 			path, file) -> {
-		 System.out.println(path.toCharArray());
-		 System.out.println(Strings.repeat("=", path.length()));
 		 try {
+			 PrintWriter writer = new PrintWriter(path.toCharArray()+".txt", "UTF-8");
+			 writer.println(path);
+			 writer.println(Strings.repeat("=", path.length()));
 			new NodeIterator(new NodeIterator.NodeHandler() {
 			   @Override
 			   public boolean handle(Node node) {
-				  return check(node);
+				  return check(node,writer);
 			   }
 			}).explore(JavaParser.parse(file));
 			new VoidVisitorAdapter<Object>() {
 				   @Override
 				   public void visit(MethodCallExpr n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("[" + n.getBeginLine() + "] " + n.getName());
+					  writer.println("[" + n.getBeginLine() + "] " + n.getName());
 				   }
 				   @Override
 				   public void visit(MethodDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getName() + " MethodDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getName() + " MethodDeclaration" );
 				   }
 				   @Override
 				   public void visit(AnnotationDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getName() + " AnnotationDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getName() + " AnnotationDeclaration" );
 				   }
 				   @Override
 				   public void visit(AnnotationMemberDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getName() + " AnnotationMemberDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getName() + " AnnotationMemberDeclaration" );
 				   }
 				   /*@Override
 				   public void visit(BodyDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.toString() + " BodyDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.toString() + " BodyDeclaration" );
 				   }*/
 				   @Override
 				   public void visit(ClassOrInterfaceDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getName() + " ClassOrInterfaceDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getName() + " ClassOrInterfaceDeclaration" );
 				   }
 				   @Override
 				   public void visit(ConstructorDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getName() + " ConstructorDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getName() + " ConstructorDeclaration" );
 				   }
 				   @Override
 				   public void visit(EmptyMemberDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.toString() + " EmptyMemberDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.toString() + " EmptyMemberDeclaration" );
 				   }
 				   @Override
 				   public void visit(EnumDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getName() + " EnumDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getName() + " EnumDeclaration" );
 				   }
 				   @Override
 				   public void visit(EmptyTypeDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getName() + " EmptyTypeDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getName() + " EmptyTypeDeclaration" );
 				   }
 				   @Override
 				   public void visit(EnumConstantDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getName() + " EnumConstantDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getName() + " EnumConstantDeclaration" );
 				   }
 				   @Override
 				   public void visit(FieldDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getType() + " FieldDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getType() + " FieldDeclaration" );
 				   }
 				   @Override
 				   public void visit(InitializerDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.toString() + " InitializerDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.toString() + " InitializerDeclaration" );
 				   }
 				   /*
 				   @Override
 				   public void visit(TypeDeclaration n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getName() + " TypeDeclaration" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getName() + " TypeDeclaration" );
 				   }*/
 				   @Override
 				   public void visit(VariableDeclarator n, Object arg) {
 					  super.visit(n, arg);
-					  System.out.println("["+n.getBeginLine()+"]" + n.getId() + " VariableDeclarator" );
+					  writer.println("["+n.getBeginLine()+"]" + n.getId() + " VariableDeclarator" );
 				   }
 				}.visit(JavaParser.parse(file), null);
-			System.out.println(); // empty line
+			writer.println(); // empty line
+			writer.close();
 		 } catch (ParseException | IOException e) {
 			new RuntimeException(e);
 		 }
 	  }).explore(projectDir);
    }
    
-   public static boolean check(Node node) { //Check statement Type //filter out statement that don,t use in this work
+   public static boolean check(Node node,PrintWriter writer) { //Check statement Type //filter out statement that don,t use in this work
 	  if (node instanceof IfStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]" + " if statement");
-		 check(((IfStmt) node).getThenStmt());
-		 check(((IfStmt) node).getElseStmt());
+		 writer.println("[" + node.getBeginLine() + "]" + " if statement");
+		 check(((IfStmt) node).getThenStmt(),writer);
+		 check(((IfStmt) node).getElseStmt(),writer);
 		 return false;
 	  } /*else if (node instanceof AssertStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println(" [" + node.getBeginLine() + "]"
 			   + " Assert statement");
 		 return false;
 	  }*/ else if (node instanceof BreakStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println("[" + node.getBeginLine() + "]"
 			   + " Break statement");
 		 return false;
 	  } else if (node instanceof ContinueStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println("[" + node.getBeginLine() + "]"
 			   + " Continue statement");
 		 return false;
 	  } else if (node instanceof DoStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]" + " Do statement");
-		 check(((DoStmt) node).getBody());
+		 writer.println("[" + node.getBeginLine() + "]" + " Do statement");
+		 check(((DoStmt) node).getBody(),writer);
 		 return false;
 	  } /*else if (node instanceof EmptyStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println(" [" + node.getBeginLine() + "]"
 			   + " Empty statement");
 		 return false;
 	  } else if (node instanceof ExplicitConstructorInvocationStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println(" [" + node.getBeginLine() + "]"
 			   + " ExplicitConstructorInvocation statement" + " "+ ((ExplicitConstructorInvocationStmt) node).getExpr());
 		 return false;
 	  } else if (node instanceof ExpressionStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println(" [" + node.getBeginLine() + "]"
 			   + " Expression statement" + " " +((ExpressionStmt) node).getExpression().getClass());
 		 return false;
 	  }*/ else if (node instanceof ForeachStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println("[" + node.getBeginLine() + "]"
 			   + " Foreach statement");
-		 check(((ForeachStmt) node).getBody());
+		 check(((ForeachStmt) node).getBody(),writer);
 		 return false;
 	  } else if (node instanceof ForStmt) {
-		 System.out
-			   .println(" [" + node.getBeginLine() + "]" + " For statement");
-		 check(((ForStmt) node).getBody());
+		 writer
+			   .println("[" + node.getBeginLine() + "]" + " For statement");
+		 check(((ForStmt) node).getBody(),writer);
 		 return false;
 	  } /*else if (node instanceof LabeledStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println(" [" + node.getBeginLine() + "]"
 			   + " Labeled statement");
 		 check(((LabeledStmt) node).getStmt());
 		 return false;
 	  }*/ else if (node instanceof ReturnStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println("[" + node.getBeginLine() + "]"
 			   + " Return statement");
 		 return false;
 	  } else if (node instanceof SwitchEntryStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println("[" + node.getBeginLine() + "]"
 			   + " SwitchEntry statement");
 		 for (Node childNode : ((SwitchEntryStmt) node).getStmts()) {
-				check(childNode);
+				check(childNode,writer);
 			 }
 		 return false;
 	  } else if (node instanceof SwitchStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println("[" + node.getBeginLine() + "]"
 			   + " Switch statement");
 		 for (Node childNode : ((SwitchStmt) node).getEntries()) {
-				check(childNode);
+				check(childNode,writer);
 			 }
 		 return false;
 	  } /*else if (node instanceof SynchronizedStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println(" [" + node.getBeginLine() + "]"
 			   + " Synchronized statement");
 		 check(((SynchronizedStmt) node).getBlock());
 		 return false;
 	  }*/ else if (node instanceof ThrowStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println("[" + node.getBeginLine() + "]"
 			   + " Throw statement");
 		 return false;
 	  } else if (node instanceof TryStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]" + " Try statement");
-		 check(((TryStmt) node).getTryBlock());
+		 writer.println("[" + node.getBeginLine() + "]" + " Try statement");
+		 check(((TryStmt) node).getTryBlock(),writer);
 		 for (CatchClause childNode : ((TryStmt) node).getCatchs()) {
-			 check(childNode.getCatchBlock());
+			 check(childNode.getCatchBlock(),writer);
 			 }
-		 check(((TryStmt) node).getFinallyBlock());
+		 check(((TryStmt) node).getFinallyBlock(),writer);
 		 return false;
 	  } /*else if (node instanceof TypeDeclarationStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println(" [" + node.getBeginLine() + "]"
 			   + " TypeDeclaration statement"+((TypeDeclarationStmt) node).getTypeDeclaration());
 		 return false;
 	  }*/ else if (node instanceof WhileStmt) {
-		 System.out.println(" [" + node.getBeginLine() + "]"
+		 writer.println("[" + node.getBeginLine() + "]"
 			   + " While statement");
-		 check(((WhileStmt) node).getBody());
+		 check(((WhileStmt) node).getBody(),writer);
 		 return false;
 	  } else if (node instanceof BlockStmt) {
 		 for (Node childNode : node.getChildrenNodes()) {
-			check(childNode);
+			check(childNode,writer);
 		 }// TODO Get all children nodes within if else for while blah2
 		 return false;
 	  } else
